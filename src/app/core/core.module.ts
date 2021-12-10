a7f1fc93d5183266351507264c102ea0b0ced01f
Ghislain <Gzeleu@gmx.de>
2021-12-10 22:54:52 +0100

    Ende auf Lession 25 , Lession 25  should be done again

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,18 @@ import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
 
 ];
 // list of component which can be used outside of this module
 const components: any = [HeaderComponent, FooterComponent, HomeComponent]
 
 @NgModule({
-  declarations: [components],
+  declarations: [components, AboutComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
