edd6607b9ef5910739e805d50c93cd300054f671
Ghislain <Gzeleu@gmx.de>
2021-12-11 19:00:02 +0100

    Fix error im test

--- src/app/shared/auth/auth.guard.spec.ts
+++ src/app/shared/auth/auth.guard.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthGuard } from './auth.guard';
 
@@ -6,9 +7,15 @@ describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule], // <==== due to  error in CI
+      providers: [],
+      declarations: [],
+    });
     guard = TestBed.inject(AuthGuard);
   });
+
+
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
