9c261262cdefca8b3752cb7a4ebca76c1e1067b1
Ghislain <Gzeleu@gmx.de>
2022-01-06 18:18:24 +0100

    Fixed erro on test

--- src/app/core/components/registry/registry.component.spec.ts
+++ src/app/core/components/registry/registry.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RegistryComponent } from './registry.component';
 
@@ -8,9 +9,10 @@ describe('RegistryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistryComponent ]
+      declarations: [RegistryComponent],
+      imports: [RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/shared/services/api.service.spec.ts
+++ src/app/shared/services/api.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ApiService } from './api.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule, RouterTestingModule] });
     service = TestBed.inject(ApiService);
   });
 
