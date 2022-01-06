89518418be3a488b9a6abb95fab9fdd7df7301f9
Ghislain <Gzeleu@gmx.de>
2022-01-06 18:25:03 +0100

    TestBed.configureTestingModule gets it

--- src/app/core/components/registry/registry.component.spec.ts
+++ src/app/core/components/registry/registry.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { RegistryComponent } from './registry.component';
@@ -11,7 +12,7 @@ describe('RegistryComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegistryComponent],
-      imports: [RouterTestingModule, HttpClientTestingModule]
+      imports: [RouterTestingModule, ReactiveFormsModule, HttpClientTestingModule]
     })
       .compileComponents();
   });
