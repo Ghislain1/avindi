eda8c9daded9942a083f7626387f0446a40ff0d4
Ghislain Z. <gzeleu@outlook.de>
2025-10-11 22:38:37 +0200

    refactor: remove unused files and update structure
    - Deleted environment.ts, main.ts, polyfills.ts, styles.scss, test.ts, tsconfig.app.json, tsconfig.json, and tsconfig.spec.json as part of project cleanup.
    - Updated index.html to enhance structure and styling, including new meta tags and improved navigation.
    - Added new CSS file for styles and custom JavaScript for enhanced functionality.
    - Implemented a new layout for the application with improved responsiveness and user experience.

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  api_url: 'https://mns237-serverapi.herokuapp.com/api/', // base Api url
-  api_url2: 'https://api.realworld.io/api' // ie. /articles
-};
